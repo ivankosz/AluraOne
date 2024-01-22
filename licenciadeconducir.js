e6b003b6ebc561dec336aab4fc4a0aa08cebe141
eivan <102188631+ivankosz@users.noreply.github.com>
2024-01-22 10:36:22 -0300

    Add files via upload

---
+++ licenciadeconducir.js
@@ -0,0 +1,16 @@
+alert ('¡Bienvenida y bievenido a nuestro sitio web!');
+
+let nombre = 'Luna';
+let edad = 25;
+let numeroDeVentas = 50;
+let saldoDisponible = 1000;
+
+alert('¡Error! Completa todos los campos');
+let mensajeDeError = '¡Error! Completa todos los campos';
+alert (mensajeDeError);
+nombre = prompt('Ingrese su nombre de Usuario:');
+edad = prompt('Ingrese su edad:');
+
+if (edad >= 18) {
+    alert('¡Puedes obtener tu licencia de conducir!')
+}
